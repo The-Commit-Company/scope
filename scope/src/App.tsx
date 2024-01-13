2d15c3f792a7160e713f2cd4f681c69815d1e959
Nikhil Kothari <nik.kothari22@live.com>
2024-01-13 17:50:18 +0530

    Episode 3: Setting up Frappe React SDK

--- scope/src/App.tsx
+++ scope/src/App.tsx
@@ -1,33 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import { FrappeProvider } from 'frappe-react-sdk'
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+
 function App() {
 	const [count, setCount] = useState(0)
 
+	// We need to pass sitename only if the Frappe version is v15 or above.
+
+	const getSiteName = () => {
+		// @ts-ignore
+		if (window.frappe?.boot?.versions?.frappe && (window.frappe.boot.versions.frappe.startsWith('15') || window.frappe.boot.versions.frappe.startsWith('16'))) {
+			// @ts-ignore
+			return window.frappe?.boot?.sitename ?? import.meta.env.VITE_SITE_NAME
+		}
+		return import.meta.env.VITE_SITE_NAME
+
+	}
+
 	return (
 		<div className="App">
-			<FrappeProvider>
-				<div>
+			<Theme
+				appearance='dark'
+				accentColor='iris'
+				panelBackground='translucent'
+			>
+				<FrappeProvider
+					socketPort={import.meta.env.VITE_SOCKET_PORT}
+					siteName={getSiteName()}
+				>
 					<div>
-						<a href="https://reactjs.org" target="_blank">
-							<img src={reactLogo} className="logo react" alt="React logo" />
-						</a>
-					</div>
-					<h1>Vite + React + Frappe</h1>
-					<div className="card">
-						<button onClick={() => setCount((count) => count + 1)}>
-							count is {count}
-						</button>
-						<p>
-							Edit <code>src/App.jsx</code> and save to test HMR
+						<h1>Vite + React + Frappe</h1>
+						<div className="card">
+							<button onClick={() => setCount((count) => count + 1)}>
+								count is {count}
+							</button>
+							<p>
+								Edit <code>src/App.jsx</code> and save to test HMR
+							</p>
+						</div>
+						<p className="read-the-docs">
+							Click on the Vite and React logos to learn more
 						</p>
 					</div>
-					<p className="read-the-docs">
-						Click on the Vite and React logos to learn more
-					</p>
-				</div>
-			</FrappeProvider>
+				</FrappeProvider>
+			</Theme>
 		</div>
 	)
 }
---
+++ scope_app/www/scope.py
@@ -0,0 +1,34 @@
+import frappe
+
+import json
+import re
+
+no_cache = 1
+
+SCRIPT_TAG_PATTERN = re.compile(r"\<script[^<]*\</script\>")
+CLOSING_SCRIPT_TAG_PATTERN = re.compile(r"</script\>")
+
+def get_context(context):
+    # csrf_token = frappe.sessions.get_csrf_token()
+    # frappe.db.commit()
+    # context.csrf_token = csrf_token
+
+    if frappe.session.user == "Guest":
+        boot = frappe.website.utils.get_boot_data()
+    else:
+        try:
+            boot = frappe.sessions.get()
+        except Exception as e:
+            raise frappe.SessionBootFailed from e
+    boot_json = frappe.as_json(boot, indent=None, separators=(",", ":"))
+    boot_json = SCRIPT_TAG_PATTERN.sub("", boot_json)
+
+    boot_json = CLOSING_SCRIPT_TAG_PATTERN.sub("", boot_json)
+    boot_json = json.dumps(boot_json)
+
+    context.update({
+        "build_version": frappe.utils.get_build_version(),
+        "boot": boot_json,
+    })
+
+    return context
